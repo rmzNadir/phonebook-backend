8d733f0237b040ef505a1496fbb6622b08a0417c
rmzNadir <dgzrz99@gmail.com>
2020-08-18 17:26:31 -0500

    Exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         name: 'Arto Hellas',
         phone: '1232123589',
-        id: 2
+        id: 1
     },
     {
         name: "John Cena",
@@ -41,6 +43,62 @@ let persons = [
 
 app.get('/api/persons',(req,res) => {
     res.json(persons);
+})
+
+app.get('/info',(req,res) => {
+   res.send(`
+    <div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        ${new Date()}
+    </div>
+   `)
+})
+
+app.get('/api/persons/:id',(req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id ===id)
+    
+    if(person!==undefined){
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(req,res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person=>person.id!==id)
+    res.status(204).end()
+})
+
+app.post('/api/persons',(req,res) => {
+  const body = req.body
+  console.log(body.name)
+  //console.log(typeof body.name)
+  //console.log(typeof persons[0].name)
+
+  const getId = () => Math.floor(Math.random()*100000000)
+  
+  if(!body.name||!body.phone){
+    return res.status(400).json({
+      error: `Something's missing!`
+    })
+  } else if(persons.some(person => person.name === body.name)){
+    return res.status(400).json({
+      error: "name must be unique!"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+    id: getId()
+  }
+  //console.log(person)
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 const PORT = 3001
