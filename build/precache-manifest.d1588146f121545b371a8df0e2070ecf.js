a8603a5a8002f055c84029769b9192bc7b07623f
rmzNadir <dgzrz99@gmail.com>
2020-09-05 14:32:06 -0500

    prod build

---
+++ build/precache-manifest.d1588146f121545b371a8df0e2070ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c4b06c53b39e78b1f4328d00944dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2bec68dccc85fd85c0",
+    "url": "/static/css/main.ecd15893.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2bec68dccc85fd85c0",
+    "url": "/static/js/main.6b0ced73.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
