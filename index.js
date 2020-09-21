b75914b4d9482679a6c149830018d1983b91abb0
rmzNadir <dgzrz99@gmail.com>
2020-09-21 13:23:49 -0500

    Exercises 3.15-3.18

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
@@ -44,50 +44,82 @@ app.use(
   })
 );
 
-
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
+  Person.find({})
+  .then((persons) => {
     res.json(persons);
-  });
+  })
+  .catch((err) => next(err));
 });
+
 
 app.get("/info", async (req, res) => {
   let count = await db.collection("people").countDocuments();
-  if(count===0){
+  if (count === 0) {
     res.send(`
       <div>
         <p>Phonebook has no info yet!</p>
         ${new Date()}
       <div>
-    `)
-  }else{
-  res.send(`
+    `);
+  } else {
+    res.send(`
     <div>
-        <p>Phonebook has info for ${count} ${count > 1 ? 'people' : 'person'}</p>
+        <p>Phonebook has info for ${count} ${
+      count > 1 ? "people" : "person"
+    }</p>
         ${new Date()}
     </div>
    `);
   }
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 
+//error handling done by middleware
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(err);
+};
 
-app.patch("/api/persons/:id", (req, res) => {
-  //console.log(req)
-  Person.findByIdAndUpdate(req.params.id, req.body).then(() =>{
-    res.json(req.body);
-  })
-})
+app.use(errorHandler); //since the execution order of middleware is the same as the order that they are loaded into express with the app.use function this is the correct place to load our error handler
 
-app.delete("/api/persons/:id", (req, res) => {
-  Person.findByIdAndRemove(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  //console.log(body)
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+    id: body.id,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((err) => next(err));
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((person) => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -97,7 +129,7 @@ app.post("/api/persons", (req, res) => {
   //console.log(typeof body.name)
   //console.log(typeof persons[0].name)
 
-  if (body.name === undefined || body.phone ===undefined) {
+  if (body.name === undefined || body.phone === undefined) {
     return res.status(400).json({ error: "Something's missing!" });
   }
 
